c576d350e5e4af79b39fb5d1e964be9c2b36a119
DeepthaGirish <deepthagirish93@gmail.com>
2017-07-07 14:14:02 -0400

    Commiting codes for 3 projects

--- ChangHui/CMakeFiles/analyze.dir/DependInfo.cmake
+++ ChangHui/CMakeFiles/analyze.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/vineeta/Programs/ChangHui_NCRCRG/ChangHui/ChangHui.cpp" "/home/vineeta/Programs/ChangHui_NCRCRG/ChangHui/CMakeFiles/analyze.dir/ChangHui.cpp.o"
+  "/home/vineeta/Programs/CCHMC_codes/ChangHui/ChangHui.cpp" "/home/vineeta/Programs/CCHMC_codes/ChangHui/CMakeFiles/analyze.dir/ChangHui.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
